da757ca370ed3041cdda4fb4dcd3065179823b7d
oxleberry <oxleberry@gmail.com>
2018-05-17 15:45:25 -0700

    added structure to about and projects sections

--- js/app.js
+++ js/app.js
@@ -1,5 +1,17 @@
 
 
+// CAROUSEL
+
+
+
+var projectTiles = [];
+var projectDetail = [{
+    title: 'Project #1',
+    img: 'images/oxle_0.png',
+    description: 'Project Description',
+    skills: ['html', 'css','js', ]
+}];
+
 $(function(){
 
 
