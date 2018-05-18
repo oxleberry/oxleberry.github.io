7b1ade85fe728fc6c010d98e0d91fc30c957bb8f
oxleberry <oxleberry@gmail.com>
2018-05-18 11:40:54 -0700

    added carousel to layout

--- js/app.js
+++ js/app.js
@@ -4,19 +4,35 @@
 // • Add functionality for projects to live update to portfolio
 // • Add functionality for testamonials to rotate
 // • Add smooth scrolling to sections
-// •
 
-var projectTiles = [];
-var projectDetail = [{
-    title: 'Project #1',
-    img: 'images/oxle_0.png',
-    description: 'Project Description',
-    skills: ['html', 'css','js']
-}
-];
+
+// document.addEventListener('DOMContentLoaded', function() {
+//   var elems = document.querySelectorAll('.carousel');
+//   var instances = M.Carousel.init(elems, options);
+// });
+
+
+// var projectTiles = [];
+// var projectDetail = [{
+//     title: 'Project #1',
+//     img: 'images/oxle_0.png',
+//     description: 'Project Description',
+//     skills: ['html', 'css','js']
+// }
+// ];
+
+
 
 $(function(){
+    console.log('test');
+    // $('.carousel').carousel();
 
-    console.log('hello');
+    $('.carousel').slick();
+
+// $('.carousel').slick({
+//   infinite: true,
+//   slidesToShow: 3,
+//   slidesToScroll: 1
+// });
 
 });  // end of document.ready
