fd84f2f163d423834c2879816abdc38acaee54a9
oxleberry <oxleberry@gmail.com>
2018-05-17 19:27:58 -0700

    added start of css styling

--- js/app.js
+++ js/app.js
@@ -1,18 +1,22 @@
 
 
-// CAROUSEL
-
-
+// • Tweak carousel of project images
+// • Add functionality for projects to live update to portfolio
+// • Add functionality for testamonials to rotate
+// • Add smooth scrolling to sections
+// •
 
 var projectTiles = [];
 var projectDetail = [{
     title: 'Project #1',
     img: 'images/oxle_0.png',
     description: 'Project Description',
-    skills: ['html', 'css','js', ]
-}];
+    skills: ['html', 'css','js']
+}
+];
 
 $(function(){
 
+    console.log('hello');
 
 });  // end of document.ready
