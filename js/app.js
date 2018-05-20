fd9cb11a6e70fbefceae3b30257c9d38072d4249
oxleberry <oxleberry@gmail.com>
2018-05-19 19:32:43 -0700

    played around with parallax scrolling

--- js/app.js
+++ js/app.js
@@ -1,24 +1,20 @@
 
 
-// • Add functionality for projects to live update to portfolio
 // • Add functionality for testamonials to rotate
-
-// generates from project list .js.
-
 
 
 $(function(){
 
     $('.carousel').slick();
 
-// $('.carousel').slick({
-//   infinite: true,
-//   slidesToShow: 3,
-//   slidesToScroll: 1
-// });
+    // $('.carousel').slick({
+    //   infinite: true,
+    //   slidesToShow: 3,
+    //   slidesToScroll: 1
+    // });
 
-// SMOOTH SCROLLING
- // https://stackoverflow.com/questions/31832227/jquery-smooth-scrolling-anchor-navigation
+    // SMOOTH SCROLLING
+    // https://stackoverflow.com/questions/31832227/jquery-smooth-scrolling-anchor-navigation
     $('header a').on('click', function(e) {
         e.preventDefault();
         var target = $(this).attr('href');
@@ -29,7 +25,7 @@ $(function(){
         if (target === '#home') {
             offsetNum = 300;
         } else {
-            offsetNum = 0;
+            offsetNum = 50;
         }
         // smooth scroll
         $('html, body').animate({
@@ -37,28 +33,26 @@ $(function(){
         }, 900);
     });
 
+    /* //www.youtube.com/watch?v=WTZpNAbz3jg&index=2&list=PLqGj3iMvMa4IyCbhul-PdeiDqmh4ooJzk */
+    // PARALLAX
+    $(window).scroll(function (){
+        // windowScroll holds the current position of where the current scroll is
+        var windowScroll = $(this).scrollTop();
+        console.log(windowScroll);
 
-    // $('#project-tiles div').eq(0).on('mouseenter', function (){
-    //     // var bg = $(this).css('background-image');
-    //     // bg = bg.replace('url(','').replace(')','').replace(/\"/gi, "");
-    //     // $('.detail-container').clear();
-    //
-    //     var title = projectDetails[0].title;
-    //     var imgEl = projectDetails[0].imgDetail;
-    //     var description = projectDetails[0].description;
-    //     var skills = projectDetails[0].skills;
-    //     // console.log(skills);
-    //     $(skills).each(function (idx){
-    //         $('.detail-container ul').append(`<li>${skills[idx]}</li>`);
-    //
-    //     });
-    //
-    //     $('.detail-container h3').text(title);
-    //     // $('.detail-img').css('background-image', 'url(images/monument-img1.png)');
-    //     $('.detail-img').css('background-image', `url(${imgEl})`);
-    //     $('.detail-container p').text(description);
-    //
-    // });
+        // $('.bg-box').css({
+        //     'transform' : 'translate(0px, -'+ windowScroll /50 + '%)'
+        // });
+        $('.midground').css({
+            'transform' : 'translate(0px, '+ windowScroll /2.5 + '%)'
+        });
+        $('.background').css({
+            'transform' : 'translate(0px, '+ windowScroll /1.9 + '%)'
+        });
+        $('.foreground').css({ 'transform' : 'translate(0px, -'+ windowScroll /14 + '%)'
+    });
+
+});
 
 
 
